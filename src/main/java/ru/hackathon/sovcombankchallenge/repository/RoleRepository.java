a6583581c51ef0747e01f1521e8a9cab37d3f479
EugeniyaMo <eugeniya.Mo@gmail.com>
2023-05-22 20:10:04 +0300

    add models User and Role

---
+++ src/main/java/ru/hackathon/sovcombankchallenge/repository/RoleRepository.java
@@ -0,0 +1,8 @@
+package ru.hackathon.sovcombankchallenge.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import javax.management.relation.Role;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+}
