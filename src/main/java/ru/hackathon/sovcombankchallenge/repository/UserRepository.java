44b8b071042b814f98fbe3a02da0f35fe90ec843
EugeniyaMo <eugeniya.Mo@gmail.com>
2023-05-23 22:51:19 +0300

    Changes by xeonstaf

--- src/main/java/ru/hackathon/sovcombankchallenge/repository/UserRepository.java
+++ src/main/java/ru/hackathon/sovcombankchallenge/repository/UserRepository.java
@@ -1,8 +1,8 @@
-package ru.hackathon.sovcombankchallenge.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import ru.hackathon.sovcombankchallenge.models.User;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-    User findByUsername(String username);
-}
+//package ru.hackathon.sovcombankchallenge.repository;
+//
+//import org.springframework.data.jpa.repository.JpaRepository;
+//import ru.hackathon.sovcombankchallenge.models.User;
+//
+//public interface UserRepository extends JpaRepository<User, Long> {
+//    User findByUsername(String username);
+//}
