978db701840f59a785a6732cc942a2b27a1518b4
EugeniyaMo <eugeniya.Mo@gmail.com>
2023-05-25 16:18:36 +0300

    fix UserRepository

--- src/main/java/ru/hackathon/sovcombankchallenge/user/repository/UserRepository.java
+++ src/main/java/ru/hackathon/sovcombankchallenge/user/repository/UserRepository.java
@@ -3,6 +3,8 @@ package ru.hackathon.sovcombankchallenge.user.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import ru.hackathon.sovcombankchallenge.user.models.User;
 
-public interface UserRepository extends JpaRepository<User, Long> {
+import java.util.UUID;
+
+public interface UserRepository extends JpaRepository<User, UUID> {
     User findByEmail(String email);
 }
