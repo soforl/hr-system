01e84908214940e150e0e69a59a577913126ffe1
Sophie <orlova.sofia2002@gmail.com>
2023-06-28 22:28:29 +0300

    fix: return code to find error with swagger

--- src/main/java/ru/hackathon/sovcombankchallenge/email/EmailService.java
+++ src/main/java/ru/hackathon/sovcombankchallenge/email/EmailService.java
@@ -1,6 +1,6 @@
-//package ru.hackathon.sovcombankchallenge.email;
-//
-//public interface EmailService {
-//    String sendSimpleMail(EmailDetails details);
-//    String sendMailWithAttachment(EmailDetails details);
-//}
+package ru.hackathon.sovcombankchallenge.email;
+
+public interface EmailService {
+    String sendSimpleMail(EmailDetails details);
+    String sendMailWithAttachment(EmailDetails details);
+}
